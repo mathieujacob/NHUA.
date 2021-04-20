0fd3f53b9df97b8d8a36742edd43a0f21874738a
Mathieu Jacob <mathieu.jacob@hotmail.fr>
2021-04-20 09:12:28 +0200

    carousel2

--- src/pages/FrenchFlow.jsx
+++ src/pages/FrenchFlow.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../App.css";
-import "../carouselle.css"
+import "../carousel.css"
 import { Link } from "react-router-dom";
 import Footer from "./../components/Footer";
 
